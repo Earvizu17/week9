cc6dea9bce5bf39cd0b43757444bf8778140fbc9
Earvizu17 <edgar.arvizu@bsu.edu>
2024-11-14 00:59:21 -0500

    week12

--- seed.js
+++ seed.js
@@ -1,23 +1,18 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
+const { PrismaClient } = require('@prisma/client')
+const prisma = new PrismaClient()
+
+
 
 const seed = async () => {
-  try {
-    const createMany = await prisma.video.createMany({
-      data: [
-        { name: 'Cat video', url: '/videos/test.mp4', votes: 10, length: 100 },
-        { name: 'Dog video', url: '/videos/test.mp4', votes: 0, length: 180 },
-        { name: 'An analysis of undersea audio structures', url: '/videos/test.mp4', votes: 2, length: 60 },
-        { name: 'Test video, please ignore', url: '/videos/test.mp4', votes: 100, length: 270 },
-      ],
-      skipDuplicates: true,
-    });
-    console.log(`Seeded ${createMany.count} videos.`);
-  } catch (error) {
-    console.error("Error seeding data:", error);
-  } finally {
-    await prisma.$disconnect();
-  }
-};
+  const createMany = await prisma.Video.createMany({
+  data: [
+            { name: 'Cat video', url: '/videos/test.mp4', votes: 10, length: 100 },
+            { name: 'Dog video', url: '/videos/test.mp4', votes: 0, length: 180 },
+  { name: 'An analsysis of undersea audio structures', url: '/videos/test.mp4', votes: 2, length: 60 },
+            { name: 'Test video, please ignore', url: '/videos/test.mp4', votes: 100, length: 270 },
+            ],
+            skipDuplicates: true
+        })
+}
 
-seed();
+seed()
